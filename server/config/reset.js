06f233330b69907426a2dff6916fdbece9c88d88
Peanut16 <Daniellekkc@gmail.com>
2026-06-24 00:22:19 -0400

    connect to the database

---
+++ server/config/reset.js
@@ -0,0 +1,93 @@
+import { pool } from "./database.js";
+import eventsData from "../data/events.js"
+import locationData from "../data/locations.js";
+
+const createLocationsTable = async () => {
+    const createLocationsTableQuery = `
+    DROP TABLE IF EXISts locations;
+    
+    CREATE TABLE locations (
+      name TEXT NOT NULL,
+      description TEXT
+    );
+    `
+    try {
+        const res = await pool.query(createTableQuery)
+        console.log('🎉 locations table created successfully')
+    } catch (err) {
+        console.error('⚠️ error creating locations table', err)
+    }
+}
+
+const createEventsTable = async () => {
+    const createEventsTableQuery = `
+    DROP TABLE IF EXISTS events
+    
+    CREATE TABLE events (
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(255) NOT NULL,
+      description TEXT,
+      date DATE NOT NULL,
+      time TEXT,
+      location_id INTEGER
+    );`
+
+    try {
+        const res = await pool.query(createTableQuery)
+        console.log('🎉 events table created successfully')
+    } catch (err) {
+        console.error('⚠️ error creating events table', err)
+    }
+}
+
+const seedLocationTable = async () => {
+    await createLocationsTable()
+
+    locationData.forEach((location) => {
+        const insertQuery = {
+            text: "INSERT INTO locations (name, description) VALUES ($1, $2)"
+        }
+
+        const values = [
+            location.name,
+            location.description
+        ]
+
+        pool.query(insertQuery, value, (err, res) => {
+            if (err) {
+                console.error('⚠️ error inserting location', err)
+                return
+            }
+            console.log(`✅ ${location.title} added successfully`)
+        })
+    })
+}
+
+const seedEventsTable = async () => {
+    await createEventsTable()
+
+    eventsData.forEach((event) => {
+        const insertQuery = {
+            text: "INSERT INTO events (title, description, date, time, location) VALUES ($1, $2, $3, $4, $5)"
+        }
+
+        const values = [
+            event.title,
+            event.description,
+            event.date,
+            event.time,
+            event.location
+        ]
+
+        pool.query(insertQuery, value, (err, res) => {
+            if (err) {
+                console.error('⚠️ error inserting event', err)
+                return
+            }
+            console.log(`✅ ${event.title} added successfully`)
+        })
+    });
+}
+
+seedLocationTable()
+seedEventsTable()
